b621327ad4392d773d89b97bca1afba0f073b573
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-02-23 05:00:31 -0600

    Integer to roman: 45 minutes

---
+++ 12_integerToRoman.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+
+
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= num <= 3999
+"""
+
+import unittest
+
+class Solution(object):
+    def intToRoman(self, num: int) -> str:
+        roman_numerals = ""
+        numerals_dict = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10 ,"IX":9,"V":5,"IV":4,"I":1}
+        for numeral_key in numerals_dict:
+            if num >= numerals_dict[numeral_key]:
+                quotient = num // numerals_dict[numeral_key]
+                num = num % numerals_dict[numeral_key]
+                for i in range(quotient):
+                    roman_numerals += numeral_key
+        return roman_numerals
+        
+        
+
+        
+class TestStringMethods(unittest.TestCase):
+
+    def test_isMatch_ex1(self):
+        inputs = 3
+        output = "III"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex2(self):
+        inputs = 58
+        output = "LVIII"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex3(self):
+        inputs = 1994
+        output = "MCMXCIV"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
+    
