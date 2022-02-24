0ff44ca82ff799a2aa1ea32060e89253bd253309
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-02-24 05:23:07 -0600

    144_145_BinaryTree preorder postorder traversal : 15 minutes

---
+++ 144_binaryTreePreorderTraversal.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+import unittest
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        vals = []
+        if not root:
+            return
+        vals.append(root.val)
+        if root.left != None:
+            vals += self.preorderTraversal(root.left)
+        if root.right != None:
+            vals += self.preorderTraversal(root.right)
+        return vals
+        
+        
+
+        
+class TestStringMethods(unittest.TestCase):
+
+    def test_isMatch_ex1(self):
+        inputs = 3
+        output = "III"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex2(self):
+        inputs = 58
+        output = "LVIII"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex3(self):
+        inputs = 1994
+        output = "MCMXCIV"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
+    
