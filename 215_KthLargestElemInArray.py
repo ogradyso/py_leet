71c63713702856d5bb570d477da56ab4cadb6314
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-02-25 16:22:49 -0600

    kth largest element in array : 15 minutes

---
+++ 215_KthLargestElemInArray.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 104
+-104 <= nums[i] <= 104
+"""
+
+import unittest
+
+from heapq import heapify, heappush, heappop
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        heapify(heap)
+        while nums:
+            heappush(heap, -1 * nums.pop())
+        for i in range(k):
+            kth_largest = heappop(heap)
+        return -1 * kth_largest
+
+        
+class TestStringMethods(unittest.TestCase):
+
+    def test_isMatch_ex1(self):
+        inputs = 3
+        output = "III"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex2(self):
+        inputs = 58
+        output = "LVIII"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex3(self):
+        inputs = 1994
+        output = "MCMXCIV"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
+    
