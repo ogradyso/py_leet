a294ab53e4dc48c365801086cefc4a97425513ce
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-02-27 18:28:40 -0600

    Validate binary search tree : did not complete

---
+++ 98_validateBinarySearchTree.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+"""
+
+import unittest
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    	stack = []
+    	prev_node = None
+    	while stack or root:
+    		while root:
+    			stack.append(root)
+    			root = root.left
+    		root = stack.pop()
+    		if prev_node and prev_node.val >= root.val: return False
+    		prev_node = root
+    		root = root.right
+    	return True
+
+
+
+        
+class TestStringMethods(unittest.TestCase):
+
+    def test_isMatch_ex1(self):
+        inputs = 3
+        output = "III"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex2(self):
+        inputs = 58
+        output = "LVIII"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex3(self):
+        inputs = 1994
+        output = "MCMXCIV"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
+    
