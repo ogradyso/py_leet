a0226f7438586ef50662ded14c95815a038e4b77
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-02 17:46:41 -0600

    max product sub array : 30 minutes

---
+++ 152_maxProductSubArray.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+A subarray is a contiguous subsequence of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+"""
+
+import unittest
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_product = max(nums)
+        current_max = 1
+        current_min = 1
+        for num in nums:
+            if num == 0:
+                current_max = 1
+                current_min = 1
+            temp = num * current_max
+            current_max = max(num * current_min, num * current_max, num)
+            current_min = min(num * current_min, temp, num)
+            max_product = max(max_product, current_max)
+        return max_product
+
+            
+
+class TestMethods(unittest.TestCase):
+
+    def test_twoSum_ex1(self):
+        nums = [2,7,11,15]
+        target = 9
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex2(self):
+        nums = [3,2,4]
+        target = 6
+        output = [1,2]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex3(self):
+        nums = [3,3]
+        target = 6
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_emptyArray(self):
+        nums = []
+        target = 6
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_unsortedArray(self):
+        nums = [9, 6, 109, 3, 1]
+        target = 10
+        output = [0,4]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_tgtNotFound(self):
+        nums = [9, 6, 109, 3, 8]
+        target = 10
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
