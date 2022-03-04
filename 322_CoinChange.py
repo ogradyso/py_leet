f32a8cc098c4e72fb02a5d7d24a71af4e6fd6eda
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-04 16:37:15 -0600

    coin change : 60 minutes

---
+++ 322_CoinChange.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+"""
+
+import unittest
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1,amount+ 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-c])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+            
+
+class TestMethods(unittest.TestCase):
+
+    def test_twoSum_ex1(self):
+        nums = [2,7,11,15]
+        target = 9
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex2(self):
+        nums = [3,2,4]
+        target = 6
+        output = [1,2]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex3(self):
+        nums = [3,3]
+        target = 6
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_emptyArray(self):
+        nums = []
+        target = 6
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_unsortedArray(self):
+        nums = [9, 6, 109, 3, 1]
+        target = 10
+        output = [0,4]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_tgtNotFound(self):
+        nums = [9, 6, 109, 3, 8]
+        target = 10
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
