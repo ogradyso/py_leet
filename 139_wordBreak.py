90a33f5b0328dcf1c2e52d66e7c585e3743ac74b
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-04 19:07:07 -0600

    word break : 60 minutes

---
+++ 139_wordBreak.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+"""
+
+import unittest
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+        for i in range(len(s)-1,-1,-1):
+            for w in wordDict:
+                if (i + len(w)) <=len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+        return dp[0]
+            
+
+class TestMethods(unittest.TestCase):
+
+    def test_twoSum_ex1(self):
+        nums = [2,7,11,15]
+        target = 9
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex2(self):
+        nums = [3,2,4]
+        target = 6
+        output = [1,2]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex3(self):
+        nums = [3,3]
+        target = 6
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_emptyArray(self):
+        nums = []
+        target = 6
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_unsortedArray(self):
+        nums = [9, 6, 109, 3, 1]
+        target = 10
+        output = [0,4]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_tgtNotFound(self):
+        nums = [9, 6, 109, 3, 8]
+        target = 10
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
