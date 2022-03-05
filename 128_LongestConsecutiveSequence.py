6b875c705e724ebde7ca99c72a0555bc57ebcaa5
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-05 16:52:00 -0600

    longest consecutive subsequence

---
+++ 128_LongestConsecutiveSequence.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+
+Constraints:
+
+1 <= text1.length, text2.length <= 1000
+text1 and text2 consist of only lowercase English characters.
+"""
+
+import unittest
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        for n in nums:
+            if (n-1) not in numSet:
+                length = 0
+                while (n+ length) in numSet:
+                    length += 1
+                longest = max(length,longest)
+        return longest
+            
+
+class TestMethods(unittest.TestCase):
+
+    def test_twoSum_ex1(self):
+        nums = [2,7,11,15]
+        target = 9
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex2(self):
+        nums = [3,2,4]
+        target = 6
+        output = [1,2]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    def test_twoSum_ex3(self):
+        nums = [3,3]
+        target = 6
+        output = [0,1]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_emptyArray(self):
+        nums = []
+        target = 6
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_unsortedArray(self):
+        nums = [9, 6, 109, 3, 1]
+        target = 10
+        output = [0,4]
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+
+    def test_twoSum_tgtNotFound(self):
+        nums = [9, 6, 109, 3, 8]
+        target = 10
+        output = []
+        mySolution = Solution()
+        self.assertEqual(mySolution.twoSum(nums, target), output)
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
