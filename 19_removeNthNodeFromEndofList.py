95ba8c08261e0710eb3f7d261fbed39ae9e7f18a
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-06 19:42:21 -0600

    Remove nth node from end of list

---
+++ 19_removeNthNodeFromEndofList.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+ 
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ 
+Follow up: Could you do this in one pass?
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+import unittest
+from typing import List
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        temp = ListNode(0, head)
+        left = temp
+        right = head
+        while n > 0 and right:
+            right = right.next
+            n-= 1
+        while right:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return temp.next
+        
+class TestMethods(unittest.TestCase):
+
+    def test_canAttendMeetings_ex1(self):
+        
+        inputs = [Interval(0,30),Interval(5,10),Interval(15,20)]
+        output = False
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+        
+    def test_canAttendMeetings_ex2(self):
+        inputs = [Interval(7,10),Interval(2,4)]
+        output = True
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+        
+    def test_canAttendMeetings_ex3(self):
+        inputs = [Interval(1,2),Interval(2,4)]
+        output = True
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
