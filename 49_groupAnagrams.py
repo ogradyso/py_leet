06b37ab7f3a27953e064e165c237533c0cde9ccd
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-08 05:36:02 -0600

    Group Anagrams

---
+++ 49_groupAnagrams.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Given an array of strings strs, group the anagrams together. 
+You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters 
+of a different word or phrase, typically using all the original 
+letters exactly once.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.v
+"""
+
+import unittest
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list) # mapping charCount to list of anagrams
+        for s in strs:
+            count = [0] * 26 # for each letter in alphabet
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            result[tuple(count)].append(s)
+        return result.values()
+    
+class TestMethods(unittest.TestCase):
+
+    def test_canAttendMeetings_ex1(self):
+        
+        inputs = [Interval(0,30),Interval(5,10),Interval(15,20)]
+        output = False
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+        
+    def test_canAttendMeetings_ex2(self):
+        inputs = [Interval(7,10),Interval(2,4)]
+        output = True
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+        
+    def test_canAttendMeetings_ex3(self):
+        inputs = [Interval(1,2),Interval(2,4)]
+        output = True
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
