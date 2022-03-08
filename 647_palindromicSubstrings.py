d66bb625e082b6230be90ed34e6f8062f5d91cc8
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-08 11:59:52 -0600

    palindromic substrings

---
+++ 647_palindromicSubstrings.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+"""
+
+import unittest
+from typing import List
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        def countPali(s, left, right):
+            res = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                res += 1
+                left -= 1
+                right += 1
+            return res
+        
+        res = 0
+        for i in range(len(s)):
+            res += countPali(s,i,i)
+            res += countPali(s,i,i+1)
+        return res
+    
+class TestMethods(unittest.TestCase):
+
+    def test_canAttendMeetings_ex1(self):
+        
+        inputs = [Interval(0,30),Interval(5,10),Interval(15,20)]
+        output = False
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+        
+    def test_canAttendMeetings_ex2(self):
+        inputs = [Interval(7,10),Interval(2,4)]
+        output = True
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+        
+    def test_canAttendMeetings_ex3(self):
+        inputs = [Interval(1,2),Interval(2,4)]
+        output = True
+        mySolution = Solution()
+        self.assertEqual(mySolution.canAttendMeetings(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
