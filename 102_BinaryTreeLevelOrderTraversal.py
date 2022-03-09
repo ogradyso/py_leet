7e40b2a6b20658a448f44fda1d0a5d11c13fb1b2
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-09 06:52:28 -0600

    Binary tree level order traversal

---
+++ 102_BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Given the root of a binary tree, return the level order 
+traversal of its nodes' values. (i.e., from left to right, 
+level by level).
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+"""
+
+import unittest
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
+        return result
+
+
+class TestMethods(unittest.TestCase):
+
+    def test_isMatch_ex1(self):
+        inputs = 3
+        output = "III"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex2(self):
+        inputs = 58
+        output = "LVIII"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+        
+    def test_isMatch_ex3(self):
+        inputs = 1994
+        output = "MCMXCIV"
+        mySolution = Solution()
+        self.assertEqual(mySolution.intToRoman(inputs), output)
+
+if __name__ == '__main__':
+    unittest.main()
+    
