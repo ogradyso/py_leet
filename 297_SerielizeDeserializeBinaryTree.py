4298ee154c6a6be91369f599dc52c5cee8511a95
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2022-03-09 20:11:33 -0600

    serialize and deserialize a binary tree

--- 297_SerielizeDeserializeBinaryTree.py
+++ 297_SerielizeDeserializeBinaryTree.py
@@ -1,51 +1,91 @@
 # -*- coding: utf-8 -*-
 """
-Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+Serialization is the process of converting a data structure 
+or object into a sequence of bits so that it can be stored 
+in a file or memory buffer, or transmitted across a network 
+connection link to be reconstructed later in the same or 
+another computer environment.
 
- 
+Design an algorithm to serialize and deserialize a binary 
+tree. There is no restriction on how your 
+serialization/deserialization algorithm should work. You 
+just need to ensure that a binary tree can be serialized 
+to a string and this string can be deserialized to the 
+original tree structure.
+
+Clarification: The input/output format is the same as how 
+LeetCode serializes a binary tree. You do not necessarily 
+need to follow this format, so please be creative and come 
+up with different approaches yourself.
 
 Example 1:
 
-
-Input: root = [1,2,2,3,4,4,3]
-Output: true
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
 Example 2:
 
-
-Input: root = [1,2,2,null,3,null,3]
-Output: false
+Input: root = []
+Output: [] 
  
-
 Constraints:
 
-The number of nodes in the tree is in the range [1, 1000].
--100 <= Node.val <= 100
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
  
 """
 
 import unittest
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def dfs(left, right):
-            if not left and not right:
-                return True
-            elif (not left) or (not right):
-                return False
-            elif left.val != right.val:
-                return False
-            if left.val == right.val:
-                return dfs(left.left, right.right) and dfs(left.right, right.left)
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
         
-        if not root:
-            return False
-        return dfs(root.left, root.right)
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = []
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = data.split(",")
+        self.i = 0
+        
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
 
 
 class TestMethods(unittest.TestCase):
