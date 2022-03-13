b3a29dde00c0499983ee5b5d2f91db3b0900e873
Shaun OGrady <shaun.m.ogrady@gmail.com>
2022-03-13 07:50:44 -0700

    min of rotated sorted array: failed first attempt 15 minutes

--- 153_minRotatedSortedArray.py
+++ 153_minRotatedSortedArray.py
@@ -41,9 +41,22 @@ nums is sorted and rotated between 1 and n times.
 import unittest
 from typing import List
 
-
-
-
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+        result = nums[0]
+        while left <= right:
+            if nums[left] < nums[right]:
+                result = min(result, num[left])
+                break
+            mid = (left + right) // 2
+            result = min(result, nums[mid])
+            if nums[mid] >= nums[left]:
+                left = mid +1
+            else:
+                right = mid -1
+        return result
             
 
 class TestMethods(unittest.TestCase):
