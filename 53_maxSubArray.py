080fdfcdde982f46e398283f08b9d631f2493675
Shaun OGrady <shaun.m.ogrady@gmail.com>
2022-03-13 06:03:23 -0700

    max sub array: failed first attempt 15 minutes

--- 53_maxSubArray.py
+++ 53_maxSubArray.py
@@ -34,7 +34,16 @@ import unittest
 from typing import List
 
 
-
+class Solution:
+    def maxSubArray(self, nums: List[int]):
+        max_val = nums[0]
+        curr_sum = 0
+        for num in nums:
+            if curr_sum < 0:
+                curr_sum = 0 
+            curr_sum += 0
+            max_val = max(max_val, curr_sum)
+        return max_val
 
 
             
